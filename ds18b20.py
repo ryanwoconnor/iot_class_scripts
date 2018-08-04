d67610552c26327ad7916560c30f0b3b90e0821d
root <ryan.oconnor@gmail.com>
2018-08-04 13:21:14 -0400

    Adding Door Sensor Script and DHT22

--- ds18b20.py
+++ ds18b20.py
@@ -1,6 +1,16 @@
 import os
 import glob
 import time
+import socket
+import configparser
+import requests
+import sys
+
+config = configparser.ConfigParser()
+config.read(os.path.join(sys.path[0], 'local/splunk_server.conf'))
+
+authHeader={'Authorization': 'Splunk '+config['DEFAULT']['token']}
+url = config['DEFAULT']['url']
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -15,7 +25,7 @@ def read_temp_raw():
     f.close()
     return lines
 
-def read_temp():
+def read_temp(url, authHeader):
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
@@ -25,6 +35,9 @@ def read_temp():
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
+        jsonDict = {'host': str(socket.gethostname()), 'event': 'metric', 'fields':{'water_temp':str(temp_f),'_value':str(temp_f),'metric_name':'water_temp'}}
+        r = requests.post(url,headers=authHeader,json=jsonDict,verify=False)
+        print(r.text)
         return "temp_c="+str(temp_c)+" "+"temp_f="+str(temp_f)
 
-print(read_temp())
+print(read_temp(url, authHeader))
