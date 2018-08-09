ec5fc412c7c333474b8e5a246af08d26fb2e28e1
Ryan O'Connor <ryan.oconnor@gmail.com>
2018-08-09 05:49:24 +0400

    Updated tplink.py script to handle not being able to connect to an IP

--- tplink.py
+++ tplink.py
@@ -77,71 +77,71 @@ for tplink in tplinks:
         sys.stderr.write('Failed to create socket')
         sys.exit()
 
-    #Connect to remote server
-    s.connect((ip, 9999))
+    try:
+        #Connect to remote server
+        s.connect((ip, 9999))
  
  
-    message = "AAAAJNDw0rfav8uu3P7Ev5+92r/LlOaD4o76k/6buYPtmPSYuMXlmA==".decode('base64')
+        message = "AAAAJNDw0rfav8uu3P7Ev5+92r/LlOaD4o76k/6buYPtmPSYuMXlmA==".decode('base64')
  
-    try :
-        #Set the whole string
-        s.sendall(message)
-    except socket.error:
-        #Send failed
-        sys.stderr.write('Send to IP '+apiKeyValue+' failed')
-        sys.exit()
+        try :
+            #Set the whole string
+            s.sendall(message)
+        except socket.error:
+            #Send failed
+            sys.stderr.write('Send to IP '+apiKeyValue+' failed')
+            sys.exit()
  
  
-    #get reply and print
-    ciphertext = recv_timeout(s)
+        #get reply and print
+        ciphertext = recv_timeout(s)
 
-    #Decode Response
-    key = 171
-    buffer = []
+        #Decode Response
+        key = 171
+        buffer = []
 
-    ciphertext = ciphertext.decode('latin-1')
-    plaintext=''
-    for char in ciphertext:
-        plain = key ^ ord(char)
-        key = ord(char)
-        buffer.append(chr(plain))
-        plaintext = ''.join(buffer)
+        ciphertext = ciphertext.decode('latin-1')
+        plaintext=''
+        for char in ciphertext:
+            plain = key ^ ord(char)
+            key = ord(char)
+            buffer.append(chr(plain))
+            plaintext = ''.join(buffer)
 
+        config = configparser.ConfigParser()
+        config.read(os.path.join(sys.path[0], 'local/splunk_server.conf'))
 
+        authHeader={'Authorization': 'Splunk '+config['DEFAULT']['token']}
+        url = config['DEFAULT']['url']
 
+        powerDict = plaintext[31:-3]
+        powerList=powerDict.split(",")
+        voltage = powerList[1].split(":")[1]
+        current = powerList[0].split(":")[1]
+        power = powerList[2].split(":")[1]
+        total = powerList[3].split(":")[1]
 
-    config = configparser.ConfigParser()
-    config.read(os.path.join(sys.path[0], 'local/splunk_server.conf'))
+        jsonDict = {'host': host, 'sourcetype': 'hs110', 'event': 'metric', 'fields':{'volts':voltage,'_value':voltage,'metric_name':'volts'}}
+        r = requests.post(url,headers=authHeader,json=jsonDict,verify=False)
+        print(jsonDict)
+        print(r.text)
 
-    authHeader={'Authorization': 'Splunk '+config['DEFAULT']['token']}
-    url = config['DEFAULT']['url']
+        jsonDict = {'host': host, 'sourcetype': 'hs110', 'event': 'metric', 'fields':{'amps':current,'_value':current,'metric_name':'amps'}}
+        r = requests.post(url,headers=authHeader,json=jsonDict,verify=False)
+        print(jsonDict)
+        print(r.text)
 
-    powerDict = plaintext[31:-3]
-    powerList=powerDict.split(",")
-    voltage = powerList[1].split(":")[1]
-    current = powerList[0].split(":")[1]
-    power = powerList[2].split(":")[1]
-    total = powerList[3].split(":")[1]
+        jsonDict = {'host': host, 'sourcetype': 'hs110', 'event': 'metric', 'fields':{'kwh':total,'_value':total,'metric_name':'kwh'}}
+        r = requests.post(url,headers=authHeader,json=jsonDict,verify=False)
+        print(jsonDict)
+        print(r.text)
 
-    jsonDict = {'host': host, 'sourcetype': 'hs110', 'event': 'metric', 'fields':{'volts':voltage,'_value':voltage,'metric_name':'volts'}}
-    r = requests.post(url,headers=authHeader,json=jsonDict,verify=False)
-    print(jsonDict)
-    print(r.text)
+        jsonDict = {'host': host, 'sourcetype': 'hs110', 'event': 'metric', 'fields':{'watts':power,'_value':power,'metric_name':'watts'}}
+        r = requests.post(url,headers=authHeader,json=jsonDict,verify=False)
+        print(jsonDict)
+        print(r.text)
 
-    jsonDict = {'host': host, 'sourcetype': 'hs110', 'event': 'metric', 'fields':{'amps':current,'_value':current,'metric_name':'amps'}}
-    r = requests.post(url,headers=authHeader,json=jsonDict,verify=False)
-    print(jsonDict)
-    print(r.text)
-
-    jsonDict = {'host': host, 'sourcetype': 'hs110', 'event': 'metric', 'fields':{'kwh':total,'_value':total,'metric_name':'kwh'}}
-    r = requests.post(url,headers=authHeader,json=jsonDict,verify=False)
-    print(jsonDict)
-    print(r.text)
-
-    jsonDict = {'host': host, 'sourcetype': 'hs110', 'event': 'metric', 'fields':{'watts':power,'_value':power,'metric_name':'watts'}}
-    r = requests.post(url,headers=authHeader,json=jsonDict,verify=False)
-    print(jsonDict)
-    print(r.text)
-
-    ##Close the socket
-    s.close()
+        ##Close the socket
+        s.close()
+    except Exception, e:
+        continue
